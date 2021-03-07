7945907fe7b76998e76c6777ebdada30b3ec7018
Sina <dalvandsina@yahoo.com>
2021-03-08 02:46:15 +0330

    chore: change main dart structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,6 @@
+import 'package:club_house/src/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            "Start of ClubHouse",
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
-      ),
-    );
-  }
 }
---
+++ lib/src/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            "Start of ClubHouse",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+}
