042b7d59da930088c49594c783d718363a9f98a0
Sina <dalvandsina@yahoo.com>
2021-03-11 01:22:15 +0330

    chore: API Call Interface creation

--- lib/src/resources/remote/apiCallInterface.dart
+++ lib/src/resources/remote/apiCallInterface.dart
@@ -1,3 +1,4 @@
+import 'package:club_house/src/models/SendSms.dart';
 import 'package:club_house/src/utils/const.dart';
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
@@ -7,4 +8,10 @@ part 'apiCallInterface.g.dart';
 @RestApi(baseUrl: Const.baseUrl)
 abstract class ApiCallInterface {
   factory ApiCallInterface(Dio dio, {String baseUrl}) = _ApiCallInterface;
+
+  @POST("/start_phone_number_auth")
+  Future<SendSms> sendSms(
+    @Part(value: "phone_number", contentType: 'application/json') String phone,
+  );
+
 }
--- lib/src/resources/remote/apiCallInterface.g.dart
+++ lib/src/resources/remote/apiCallInterface.g.dart
@@ -9,10 +9,32 @@ part of 'apiCallInterface.dart';
 class _ApiCallInterface implements ApiCallInterface {
   _ApiCallInterface(this._dio, {this.baseUrl}) {
     ArgumentError.checkNotNull(_dio, '_dio');
-    baseUrl ??= 'https://5d42a6e2bc64f90014a56ca0.mockapi.io/api/v1/';
+    baseUrl ??= 'https://www.clubhouseapi.com/api';
   }
 
   final Dio _dio;
 
   String baseUrl;
+
+  @override
+  Future<SendSms> sendSms(phone) async {
+    ArgumentError.checkNotNull(phone, 'phone');
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = FormData();
+    if (phone != null) {
+      _data.fields.add(MapEntry('phone_number', phone));
+    }
+    final _result = await _dio.request<Map<String, dynamic>>(
+        '/start_phone_number_auth',
+        queryParameters: queryParameters,
+        options: RequestOptions(
+            method: 'POST',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            baseUrl: baseUrl),
+        data: _data);
+    final value = SendSms.fromJson(_result.data);
+    return value;
+  }
 }
