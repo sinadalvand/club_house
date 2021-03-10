6cbc14e539d7071f0bddbaeee546f78ec710762a
Sina <dalvandsina@yahoo.com>
2021-03-11 00:23:22 +0330

    chore: base contracts add

---
+++ lib/src/contracts/controller.dart
@@ -0,0 +1,5 @@
+import 'package:get/get.dart';
+
+class Controller extends GetxController {
+  
+}
---
+++ lib/src/contracts/repository.dart
@@ -0,0 +1,9 @@
+import 'package:club_house/src/resources/remote/apiCallInterface.dart';
+import 'package:get/get.dart';
+
+class Repository {
+  /* retrofit instance */
+  ApiCallInterface api = Get.find();
+
+
+}
