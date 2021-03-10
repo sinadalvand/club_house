9d6dba12c884549ae10996314adc964f8ba77c1b
Sina <dalvandsina@yahoo.com>
2021-03-11 00:26:02 +0330

    chore: SMS Send model creation

---
+++ lib/src/models/SendSms.dart
@@ -0,0 +1,16 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'SendSms.g.dart';
+
+@JsonSerializable()
+class SendSms {
+  String success;
+  bool is_blocked;
+  String error_message;
+
+  SendSms(this.success, this.is_blocked, this.error_message);
+
+  factory SendSms.fromJson(Map<String, dynamic> json) => _$SendSmsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SendSmsToJson(this);
+}
---
+++ lib/src/models/SendSms.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'SendSms.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SendSms _$SendSmsFromJson(Map<String, dynamic> json) {
+  return SendSms(
+    json['success'] as String,
+    json['is_blocked'] as bool,
+    json['error_message'] as String,
+  );
+}
+
+Map<String, dynamic> _$SendSmsToJson(SendSms instance) => <String, dynamic>{
+      'success': instance.success,
+      'is_blocked': instance.is_blocked,
+      'error_message': instance.error_message,
+    };
