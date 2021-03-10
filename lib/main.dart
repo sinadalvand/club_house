56338d2dfaca22015d07828d2f80a919b6253b7f
Sina <dalvandsina@yahoo.com>
2021-03-11 00:24:48 +0330

    chore: add simple logger

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:club_house/src/app.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
+var logger = Logger();
 void main() {
   runApp(MyApp());
 }
