c05af7c4b1feed4d06cb3be09d931e4dcc4a48ae
Sina <dalvandsina@yahoo.com>
2021-04-19 20:17:11 +0430

    chore: profile repository injection

--- lib/src/di/repository_injection.dart
+++ lib/src/di/repository_injection.dart
@@ -1,8 +1,10 @@
 import 'package:club_house/src/repository/AuthRepository.dart';
+import 'package:club_house/src/repository/ProfileRepository.dart';
 import 'package:get/get.dart';
 
 final Function repository_inject = () {
 
-  Get.lazyPut(() => AuthRepository());
+  Get.lazyPut(() => AuthRepository(),fenix: true);
+  Get.lazyPut(() => ProfileRepository(),fenix: true);
 
 };
