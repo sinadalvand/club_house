74a64c2982e26ffc39f1478562eed894280416c5
Sina <dalvandsina@yahoo.com>
2021-04-19 20:15:34 +0430

    fix: useless import and function remove

--- lib/src/repository/AuthRepository.dart
+++ lib/src/repository/AuthRepository.dart
@@ -2,15 +2,13 @@ import 'dart:core';
 
 import 'package:club_house/src/contracts/repository.dart';
 import 'package:club_house/src/models/SendSms.dart';
-import 'package:club_house/src/models/Session.dart';
 import 'package:club_house/src/models/VerifySms.dart';
+import 'package:club_house/src/models/WaitList.dart';
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
 class AuthRepository extends Repository {
   Dio dio = Get.find();
-
-  Session session = Get.find();
 
   Future<SendSms> sendSms(String phone) => api.sendSms(phone);
 
@@ -19,4 +17,6 @@ class AuthRepository extends Repository {
   Future<SendSms> makeCall(String phone) => api.makeCall(phone);
 
   Future<VerifySms> verifyCode(String phone,String code) => api.verifyCode(phone,code);
+
+  Future<WaitList> isWaitList() => api.isWaitList();
 }
