d3e540b437fa663d02275db708917628c92d1523
Sina <dalvandsina@yahoo.com>
2021-04-26 22:37:42 +0430

    fix: request headers fix

--- lib/src/di/inject.dart
+++ lib/src/di/inject.dart
@@ -10,22 +10,21 @@ import 'package:get_storage/get_storage.dart';
 
 class Inject {
   inject() async {
-    Get.put(GetStorage(),permanent: true);
+    Get.put(GetStorage(), permanent: true);
     Get.put(Valutor(), permanent: true);
     Get.put(Session(Get.find())..load(), permanent: true);
-    Get.put(_getDio(Get.find()), permanent: true);
+    Get.put(_getDio(Get.find(), Get.find()), permanent: true);
     Get.put(ApiCallInterface(Get.find()), permanent: true);
 
     // inject repositories
     repository_inject();
-
   }
 
-  Dio _getDio(Session session) {
+  Dio _getDio(Session session, Valutor valutor) {
     // httpClient.
 
     final dio = Dio()
-      ..httpClientAdapter  = SmartHttpClientAdapter()
+      ..httpClientAdapter = SmartHttpClientAdapter()
       ..interceptors.add(InterceptorsWrapper(onRequest: (RequestOptions options) async {
         var customHeaders = {
           'content-type': 'application/json',
@@ -35,8 +34,13 @@ class Inject {
           'CH-AppBuild': Const.API_BUILD_ID,
           'CH-AppVersion': Const.API_BUILD_VERSION,
           'User-Agent': 'clubhouse/' + Const.API_BUILD_ID + ' (iPhone; iOS 13.5.1; Scale/3.00)',
-          'CH-DeviceId': session.token,
+          'CH-DeviceId': session.deviceId,
         };
+
+        if (valutor.token != null) customHeaders['Authorization'] = "Token " + valutor.token;
+
+        if (valutor.user_id != null) customHeaders['CH-UserID'] = valutor.user_id.toString();
+
         options.headers.addAll(customHeaders);
         return options;
       }))
